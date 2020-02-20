04983f212bf409aff9c7402f819e709ac2878ff6
于海军 <yuhaijun@yuhaijundeMac.local>
2020-02-20 03:48:55 -0800

    emacs.d

---
+++ .emacs.d/elpa/ivy-hydra-20191018.1251/ivy-hydra-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ivy-hydra" "20191018.1251" "Additional key bindings for Ivy" '((emacs "24.5") (ivy "0.13.0") (hydra "0.15.0")) :commit "98a29b610a96e7d4c87d08637e8bc3fd9d206d11" :keywords '("convenience") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
